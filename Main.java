57b8c98a0d21b0ae98cdc9c31b0a4595b2f9c469
Rafael Timoteo <rafa.timoteo200@gmail.com>
2023-07-09 03:45:43 -0300

    Classe que transforma letra em maiuscula finalizada - quarto código do desafio

--- Main.java
+++ Main.java
@@ -4,15 +4,24 @@ public class Main {
         char[] frase = "Hello, world! OpenAI is amazing.".toCharArray();
         RevertePalavras.revertePalavras(frase);
         System.out.println("Frase invertida: " + String.valueOf(frase));
+        System.out.println("\n");
 
         // Teste da classe CaractereDuplicado
         char[] string = "Hello World!".toCharArray();
         CaractereDuplicado.caractereDuplicado(string);
         System.out.println("String sem caracteres duplicados: " + String.valueOf(string));
+        System.out.println("\n");
 
         // Teste da classe PalindromoLongo
         String palavra = "babad";
         String palindromo = PalindromoLongo.palindromoLongo(palavra);
-        System.out.println("Maior palíndromo encontrado: " + palindromo);
+        System.out.println("Maior palindromo encontrado: " + palindromo);
+        System.out.println("\n");
+
+        // Teste da classe LetraMaiuscula
+        char[] frase2 = "hello. how are you? i'm fine, thank you.".toCharArray();
+        LetraMaiuscula.letraMaiuscula(frase2);
+        System.out.println("String com a primeira letra de cada palavra em maiúscula: " + String.valueOf(frase2));
+        System.out.println("\n");
     }
 }
---
+++ letraMaiuscula.java
@@ -0,0 +1,22 @@
+public class LetraMaiuscula {
+    /**
+     * Transforma todas as primeiras letras de palavras em maiuscula.
+     * @param string A string a ser processada.
+     */
+    public static void letraMaiuscula(char[] string) {    
+        int tamanhoString = string.length;
+        boolean proximaLetra = true;
+
+        for (int contador = 0; contador < tamanhoString; contador++) {
+            if (proximaLetra && Character.isLetter(string[contador])) {
+                // Transforma a letra para maiúscula
+                string[contador] = Character.toUpperCase(string[contador]);
+                proximaLetra = false;
+            } else if (string[contador] == ' ') {
+                // Define a variável "proximaLetra" como verdadeira quando encontrar os caracteres ".", "!" ou "?"
+                proximaLetra = true;
+            }
+        }
+    }
+    
+}
